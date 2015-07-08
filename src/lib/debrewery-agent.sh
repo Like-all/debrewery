f54a7a7cd47cc7f3866a952a8effc0e63fa8906f
Azer Abdullaev <buckstabu@yandex-team.ru>
2015-07-08 10:59:24 +0300

    post-receive hook and agent

---
+++ src/lib/debrewery-agent.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+while getopts "d:a:u:r:m:" opts; do
+    case $opts in
+        d)
+            DISTRO=$OPTARG
+            ;;
+        a)
+            ARCH=$OPTARG
+            ;;
+        u)
+            URL=$OPTARG
+            ;;
+        r)
+            REPO=$OPTARG
+            ;;
+        m)
+            COMMITMESSAGE=$OPTARG
+            ;;
+    esac
+done
+
+BUILDHOST=`echo $URL | cut -f 1 -d '/'`
+
+echo -e '\e[0;32m'$DISTRO'/'$ARCH'\e[0m'
+cd  /root/dev
+rm *.deb *.?z *.build *.upload *.changes *.dsc
+if [ -d $REPO ]; then
+    cd $REPO
+    unset GIT_DIR
+    git reset --hard
+    git pull
+    git submodule update --recursive
+else
+    git clone --recursive $URL && cd $REPO
+fi
+debclean > /dev/null
+source ./debian/package.sh
+if echo $FLAVOURS | grep -oq $DISTRO; then
+    if [ -f ./autogen.sh ]; then
+        ./autogen.sh
+    fi
+    dch --preserve --newversion `dpkg-parsechangelog | grep Version | cut -f 2 -d ' '`"+"$DISTRO ""
+    dch --preserve -D $DISTRO --force-distribution ""
+    program_version=`dpkg-parsechangelog | grep Version | cut -f 2 -d ' ' | cut -f 1 -d '-'`
+    echo -e "\e[0;31m$NAME_$program_version\e[0m"
+    dh_make --createorig -s -y -p $NAME"_"$program_version
+    if debuild -eDEB_BUILD_OPTIONS="parallel=32" > /tmp/debuild.log 2>&1; then
+        echo -e '\e[0;34mbuild succeeded\e[0m'
+        lintian_errors=$(more +/'running lintian' /tmp/debuild.log | grep 'E:' | wc -l)
+        lintian_warnings=$(more +/'running lintian' /tmp/debuild.log | grep 'W:' | wc -l)
+        #ssh $BUILDHOST buildinfo $DISTRO $ARCH succeeded
+        #ssh $BUILDHOST lintianinfo $REPO $lintian_errors $lintian_warnings
+    else
+        echo -e '\e[0;31mbuild failed'
+        #ssh $BUILDHOST buildinfo $DISTRO $ARCH failed
+        tail -n 15 /tmp/debuild.log
+        echo -e '\e[0m'
+    fi
+    dupload --nomail /root/dev/*.changes
+fi
