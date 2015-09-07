3c285e3411e8c1965ae4b32e199f78d7f901fd62
Like-all <like-all@yandex.com>
2015-09-07 18:53:47 +0300

    fix a typo

--- debian/package.sh
+++ debian/package.sh
@@ -1,4 +1,4 @@
-export DEBFULLNAME="Azer Abdullaev (Like All)"
-export DEBEMAIL="like-all@yandex.com"
+export DEBFULLNAME="Azer Abdullaev"
+export DEBEMAIL="buckstabu@yandex-team.ru"
 NAME="debrewery"
 FLAVOURS="any"
